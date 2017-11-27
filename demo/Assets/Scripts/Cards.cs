cdde0f0217b757c190083520c5327d7c231aadd1
tvledesign <tvledesign@gmail.com>
2017-11-27 01:12:13 -0600

    Added Card Classes
    Added card classes to hold individual card data.

---
+++ demo/Assets/Scripts/Cards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Cards
+/// Description: Properties for each individual card.
+/// </summary>
+
+[System.Serializable]
+public class Cards
+{
+    public string suit;
+    public string color;
+    public string rank;
+}
+
+/// <summary>
+/// ListOfCards
+/// Description: Contains a list of cards.
+/// </summary>
+
+[System.Serializable]
+public class ListOfCards
+{
+    public List<Cards> deck;
+}
