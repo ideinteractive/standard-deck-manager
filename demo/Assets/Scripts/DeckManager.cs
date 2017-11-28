11959613843020a1c82c6c97c594f0bef228c30c
tvledesign <tvledesign@gmail.com>
2017-11-28 15:19:23 -0600

    Added DeckManager Functionality
    You can now:
    
    - Move Cards Up, Down, To Top and To Bottom
    - Add Cards to Deck or Discard Pile
    - Remove Card
    - Shuffle Deck, Discard Pile, Discard Pile to Top of Deck, Discard Pile to Bottom of Deck or Deck with Discard Pile

--- demo/Assets/Scripts/DeckManager.cs
+++ demo/Assets/Scripts/DeckManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 
 /// <summary>
 /// DeckManager
@@ -58,25 +59,69 @@ public class DeckManager : MonoBehaviour
     // move a card up the deck
     public void MoveCardUp(Cards card, List<Cards> deck)
     {
+        // get the current index of the card
+        int index = deck.IndexOf(card);
 
+        // if that index is not zeo
+        if(index != 0)
+        {
+            // remove the current card from the deck
+            deck.Remove(card);
+
+            // add the card into the position
+            deck.Insert(index - 1, card);
+        }
     }
 
     // move a card down the deck
     public void MoveCardDown(Cards card, List<Cards> deck)
     {
+        // get the current index of the card
+        int index = deck.IndexOf(card);
 
+        // if that index is not the max count of the deck
+        if (index != (deck.Count - 1))
+        {
+            // remove the current card from the deck
+            deck.Remove(card);
+
+            // add the card into the position
+            deck.Insert(index + 1, card);
+        }
     }
 
     // move a card to the top of the deck
     public void MoveCardToTop(Cards card, List<Cards> deck)
     {
+        // get the current index of the card
+        int index = deck.IndexOf(card);
 
+        // if that index is not zero
+        if (index != 0)
+        {
+            // remove the current card from the deck
+            deck.Remove(card);
+
+            // add the card to the top of the deck
+            deck.Insert(0, card);
+        }
     }
 
     // move a card to the bottom of the deck
     public void MoveCardToBottom(Cards card, List<Cards> deck)
     {
+        // get the current index of the card
+        int index = deck.IndexOf(card);
 
+        // if that index is not the max count of the deck
+        if (index != (deck.Count - 1))
+        {
+            // remove the current card from the deck
+            deck.Remove(card);
+
+            // add the card to the bottom of the deck
+            deck.Insert(deck.Count, card);
+        }
     }
 
     // add card to the deck
@@ -100,6 +145,8 @@ public class DeckManager : MonoBehaviour
     // add a new card to the deck
     public void AddNewCard(List<Cards> deck)
     {
+        // create a new card with default values
+        // and add it to the deck
         Cards card = new Cards();
         card.strName = "Black Ace of Clubs";
         card.color = Cards.Color.Black;
@@ -111,24 +158,80 @@ public class DeckManager : MonoBehaviour
     // remove a card from the deck
     public void RemoveCard(Cards card, List<Cards> deck)
     {
+        // remove the card from the deck
         deck.Remove(card);
     }
 
     // shuffle the deck
     public void Shuffle()
     {
+        // shuffle the deck
+        RandomizeList(deck);
+    }
 
+    // shuffle the discard pile
+    public void ShuffleDiscardPile()
+    {
+        // shuffle the discard pile
+        RandomizeList(discardPile);
     }
 
     // shuffle the deck with the discard pile
     public void ShuffleWithDiscard()
     {
+        // add the discard pile to the deck
+        deck.AddRange(discardPile);
 
+        // shuffle the deck
+        RandomizeList(deck);
+
+        // clear the discard pile
+        discardPile = new List<Cards>();
+    }
+
+    // shuffle the discard pile and add it to the top of the deck
+    public void ShuffleDiscardToTop()
+    {
+        // shuffle the discard pile
+        RandomizeList(discardPile);
+
+        // add the deck to the bottom of the discard pile
+        discardPile.AddRange(deck);
+
+        // clear the deck
+        deck = new List<Cards>();
+
+        // set the deck to the discard pile
+        deck = discardPile;
+
+        // clear the discard pile
+        discardPile = new List<Cards>();
     }
 
     // shuffle the discard pile and add it to the bottom of the deck
     public void ShuffleDiscardToBottom()
     {
+        // shuffle the discard pile
+        RandomizeList(discardPile);
 
+        // add it to the bottom of the deck
+        deck.AddRange(discardPile);
+
+        // clear the discard pile
+        discardPile = new List<Cards>();
+    }
+
+    // randomize a list that has been passed through
+    private static void RandomizeList(List<Cards> list)
+    {
+        // for each card in the list
+        for (int i = 0; i < list.Count; i++)
+        {
+            // randomize the order
+            Cards temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 } 
