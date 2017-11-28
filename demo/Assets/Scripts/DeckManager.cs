63c25224dc6185207e4dc9cb8ef7fafd2e86f4a2
tvledesign <tvledesign@gmail.com>
2017-11-28 02:40:56 -0600

    Added Empty Functions
    Added empty functions for possible functionality.

--- demo/Assets/Scripts/DeckManager.cs
+++ demo/Assets/Scripts/DeckManager.cs
@@ -39,6 +39,12 @@ public class DeckManager : MonoBehaviour
         // create a new deck based on that list
         deck = new List<Cards>(listOfCards.deck);
 
+        // for each card in the deck assign a name
+        foreach (Cards card in deck)
+        {
+            card.strName = card.color.ToString() + " " + card.rank.ToString() + " of " + card.suit.ToString();
+        }
+
         // create a new discard pile
         discardPile = new List<Cards>();
 
@@ -48,4 +54,55 @@ public class DeckManager : MonoBehaviour
         initialized = true;
     }
     #endif
+
+    // move a card up the deck
+    public void MoveCardUp(Cards card)
+    {
+
+    }
+
+    // move a card down the deck
+    public void MoveCardDown(Cards card)
+    {
+
+    }
+
+    // move a card to the top of the deck
+    public void MoveCardToTop(Cards card)
+    {
+
+    }
+
+    // move a card to the bottom of the deck
+    public void MoveCardToBottom(Cards card)
+    {
+
+    }
+
+    // discard a card to the discard pile
+    public void DiscardCard(Cards card)
+    {
+        // add the card to the discard pille
+        // and remove it from the deck
+        discardPile.Add(card);
+        deck.Remove(card);
+    }
+
+    // shuffle the deck
+    public void Shuffle()
+    {
+
+    }
+
+    // shuffle the deck with the discard pile
+    public void ShuffleWithDiscard()
+    {
+
+    }
+
+    // shuffle the discard pile and add it to the bottom of the deck
+    public void ShuffleDiscard()
+    {
+
+    }
 } 
