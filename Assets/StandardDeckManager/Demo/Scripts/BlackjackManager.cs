5e790885510dbd16d01d40cc701201f58dd50c08
Tony V. Le <tvledesign@gmail.com>
2019-02-07 02:21:41 -0600

    Update BlackjackManager.cs

--- Assets/StandardDeckManager/Demo/Scripts/BlackjackManager.cs
+++ Assets/StandardDeckManager/Demo/Scripts/BlackjackManager.cs
@@ -353,6 +353,7 @@ public class BlackjackManager : MonoBehaviour
         // if the hand contains an ace
         if (t_blnContainsAce)
         {
+
             // if the hand is not greater than 21 set the ace value to 11
             if ((t_intScore - 1) + 11 <= 21)
             {
