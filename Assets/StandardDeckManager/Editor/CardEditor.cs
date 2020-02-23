3c60d502df00f53b04bed8232f0996c381057fdf
Tony V. Le <tvledesign@gmail.com>
2020-02-23 01:43:56 -0600

    Update CardEditor.cs

--- Assets/StandardDeckManager/Editor/CardEditor.cs
+++ Assets/StandardDeckManager/Editor/CardEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using StandardDeckManager.Scripts;
 using UnityEditor;
 using UnityEngine;
@@ -12,38 +13,35 @@ namespace StandardDeckManager.Editor
 
     public class CardEditor : EditorWindow
     {
-        public static CardEditor instance { get; private set; } // create a reference to this editor
-        private static DeckManagerEditor deckManagerEditor; // reference our deck manager editor
+        public static CardEditor Instance { get; private set; } // create a reference to this editor
+        private static DeckManagerEditor _deckManagerEditor; // reference our deck manager editor
 
         // check if this window is open or not
-        public static bool IsOpen
-        {
-            get { return instance != null; }
-        }
+        public static bool IsOpen => Instance != null;
 
-        public int intCardIndex;    // reference our card index
+        public int intCardIndex; // reference our card index
         public bool blnEditingCardFromDeck; // checks if we are editing from the deck
-        public bool blnEditingCardFromDiscard;  // checks if we are editing from the discard
-        public bool blnEditingCardFromInUse;    // checks if we are editing from the in use
+        public bool blnEditingCardFromDiscard; // checks if we are editing from the discard
+        public bool blnEditingCardFromInUse; // checks if we are editing from the in use
 
         // on initialization
         [MenuItem("Standard Deck Manager/Card Editor")]
         private static void Init()
         {
             // get existing open window or if none, make a new one
-            CardEditor window = (CardEditor)EditorWindow.GetWindow(typeof(CardEditor), false, "Card Editor");
+            CardEditor window = (CardEditor) EditorWindow.GetWindow(typeof(CardEditor), false, "Card Editor");
             window.minSize = new Vector2(325, 140);
             window.Show();
 
             // set the reference to the current inspected object
-            deckManagerEditor = DeckManagerEditor.Instance;
+            _deckManagerEditor = DeckManagerEditor.Instance;
         }
 
         // on enable
         private void OnEnable()
         {
-            instance = this;    // set this instance
-            deckManagerEditor = DeckManagerEditor.Instance; // set the reference to the current inspected object
+            Instance = this; // set this instance
+            _deckManagerEditor = DeckManagerEditor.Instance; // set the reference to the current inspected object
         }
 
         // repaint the inspector if it gets updated
@@ -58,55 +56,72 @@ namespace StandardDeckManager.Editor
             // try to draw our fields and if we can't turn them off
             try
             {
-                EditorGUILayout.HelpBox("Changing or editing a card with the Card Editor has no impact during play mode.", MessageType.Info);
+                EditorGUILayout.HelpBox(
+                    "Changing or editing a card with the Card Editor has no impact during play mode.",
+                    MessageType.Info);
 
                 EditorGUILayout.Space();
 
                 // assign the color and rank
-                deck[intCardIndex].color = (Card.Color)EditorGUILayout.EnumPopup("Color", deck[intCardIndex].color);
-                deck[intCardIndex].rank = (Card.Rank)EditorGUILayout.EnumPopup("Rank", deck[intCardIndex].rank);
+                deck[intCardIndex].color = (Card.Color) EditorGUILayout.EnumPopup("Color", deck[intCardIndex].color);
+                deck[intCardIndex].rank = (Card.Rank) EditorGUILayout.EnumPopup("Rank", deck[intCardIndex].rank);
 
-                // set up the correct suit based on our card suit
-                if (deck[intCardIndex].suit == Card.Suit.Clubs)
-                    deck[intCardIndex].blackSuit = Card.BlackSuit.Clubs;
-                else if (deck[intCardIndex].suit == Card.Suit.Spades)
-                    deck[intCardIndex].blackSuit = Card.BlackSuit.Spades;
-                if (deck[intCardIndex].suit == Card.Suit.Diamonds)
-                    deck[intCardIndex].redSuit = Card.RedSuit.Diamonds;
-                else if (deck[intCardIndex].suit == Card.Suit.Hearts)
-                    deck[intCardIndex].redSuit = Card.RedSuit.Hearts;
+                switch (deck[intCardIndex].suit)
+                {
+                    // set up the correct suit based on our card suit
+                    case Card.Suit.Clubs:
+                        deck[intCardIndex].blackSuit = Card.BlackSuit.Clubs;
+                        break;
+                    case Card.Suit.Spades:
+                        deck[intCardIndex].blackSuit = Card.BlackSuit.Spades;
+                        break;
+                    case Card.Suit.Diamonds:
+                        deck[intCardIndex].redSuit = Card.RedSuit.Diamonds;
+                        break;
+                    case Card.Suit.Hearts:
+                        deck[intCardIndex].redSuit = Card.RedSuit.Hearts;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
 
                 // if our color is red
                 if (deck[intCardIndex].color == Card.Color.Red)
                 {
-                    deck[intCardIndex].redSuit = (Card.RedSuit)EditorGUILayout.EnumPopup("Suit", deck[intCardIndex].redSuit);
+                    deck[intCardIndex].redSuit =
+                        (Card.RedSuit) EditorGUILayout.EnumPopup("Suit", deck[intCardIndex].redSuit);
 
                     // assign our suit base on the selection
-                    if(deck[intCardIndex].redSuit == Card.RedSuit.Diamonds)
-                        deck[intCardIndex].suit = Card.Suit.Diamonds;
-                    else
-                        deck[intCardIndex].suit = Card.Suit.Hearts;
-                } else
+                    deck[intCardIndex].suit = deck[intCardIndex].redSuit == Card.RedSuit.Diamonds
+                        ? Card.Suit.Diamonds
+                        : Card.Suit.Hearts;
+                }
+                else
                 {
-                    deck[intCardIndex].blackSuit = (Card.BlackSuit)EditorGUILayout.EnumPopup("Suit", deck[intCardIndex].blackSuit);
+                    deck[intCardIndex].blackSuit =
+                        (Card.BlackSuit) EditorGUILayout.EnumPopup("Suit", deck[intCardIndex].blackSuit);
 
                     // assign our suit base on the selection
-                    if (deck[intCardIndex].blackSuit == Card.BlackSuit.Clubs)
-                        deck[intCardIndex].suit = Card.Suit.Clubs;
-                    else
-                        deck[intCardIndex].suit = Card.Suit.Spades;
+                    deck[intCardIndex].suit = deck[intCardIndex].blackSuit == Card.BlackSuit.Clubs
+                        ? Card.Suit.Clubs
+                        : Card.Suit.Spades;
                 }
 
                 deck[intCardIndex].value = EditorGUILayout.IntField("Value", deck[intCardIndex].value);
-                deck[intCardIndex].card = (GameObject)EditorGUILayout.ObjectField("Card", deck[intCardIndex].card, typeof(GameObject), true);
-                deck[intCardIndex].blnAutoAssign = EditorGUILayout.Toggle("Auto Assign", deck[intCardIndex].blnAutoAssign);
-            
+                deck[intCardIndex].card =
+                    (GameObject) EditorGUILayout.ObjectField("Card", deck[intCardIndex].card, typeof(GameObject), true);
+                deck[intCardIndex].blnAutoAssign =
+                    EditorGUILayout.Toggle("Auto Assign", deck[intCardIndex].blnAutoAssign);
+
                 // if auto assign is true automatically assign an object based on selection
-                if(deck[intCardIndex].blnAutoAssign)
-                    deck[intCardIndex].card = Resources.Load("Prefabs/" + deck[intCardIndex].color + " " + deck[intCardIndex].rank + " " + deck[intCardIndex].suit) as GameObject;
+                if (deck[intCardIndex].blnAutoAssign)
+                    deck[intCardIndex].card =
+                        Resources.Load("Prefabs/" + deck[intCardIndex].color + " " + deck[intCardIndex].rank + " " +
+                                       deck[intCardIndex].suit) as GameObject;
 
                 DeckManagerEditor.Instance.EditCard(intCardIndex, deck[intCardIndex], deck);
-            } catch
+            }
+            catch
             {
                 blnEditingCardFromDeck = false;
                 blnEditingCardFromDiscard = false;
@@ -116,14 +131,14 @@ namespace StandardDeckManager.Editor
         }
 
         // create a texture background
-        public static Texture2D SetBackground(int width, int height, Color color)
+        private static Texture2D SetBackground(int width, int height, Color color)
         {
-            Color[] pixels = new Color[width * height];
+            var pixels = new Color[width * height];
 
-            for (int i = 0; i < pixels.Length; i++)
+            for (var i = 0; i < pixels.Length; i++)
                 pixels[i] = color;
 
-            Texture2D result = new Texture2D(width, height);
+            var result = new Texture2D(width, height);
             result.SetPixels(pixels);
             result.Apply();
 
@@ -134,11 +149,11 @@ namespace StandardDeckManager.Editor
         private void OnGUI()
         {
             // header styles
-            GUIStyle styleRowHeader = new GUIStyle
+            var styleRowHeader = new GUIStyle
             {
-                padding = new RectOffset(0, 0, 3, 3)
+                padding = new RectOffset(0, 0, 3, 3),
+                normal = {background = SetBackground(1, 1, new Color(0.1f, 0.1f, 0.1f, 0.2f))}
             };
-            styleRowHeader.normal.background = SetBackground(1, 1, new Color(0.1f, 0.1f, 0.1f, 0.2f));
 
             EditorGUILayout.Space();
             EditorGUILayout.BeginHorizontal(styleRowHeader);
@@ -150,7 +165,9 @@ namespace StandardDeckManager.Editor
             {
                 try
                 {
-                    EditorGUILayout.HelpBox("There is no card currently selected. Please select a card from the Deck Manager to edit.", MessageType.Info);
+                    EditorGUILayout.HelpBox(
+                        "There is no card currently selected. Please select a card from the Deck Manager to edit.",
+                        MessageType.Info);
                 }
                 catch
                 {
@@ -159,12 +176,12 @@ namespace StandardDeckManager.Editor
             }
 
             // if the deck manager editor is null 
-            if (deckManagerEditor == null)
+            if (_deckManagerEditor == null)
             {
                 // try and find a reference to the deck manager editor
                 if (DeckManagerEditor.Instance)
                 {
-                    deckManagerEditor = DeckManagerEditor.Instance;
+                    _deckManagerEditor = DeckManagerEditor.Instance;
 
                 }
                 else
@@ -178,11 +195,11 @@ namespace StandardDeckManager.Editor
 
             // if we are editing the card
             if (blnEditingCardFromDeck)
-                DrawFields(deckManagerEditor.deckManager.deck);
+                DrawFields(_deckManagerEditor.deckManager.deck);
             else if (blnEditingCardFromDiscard)
-                DrawFields(deckManagerEditor.deckManager.discardPile);
+                DrawFields(_deckManagerEditor.deckManager.discardPile);
             else if (blnEditingCardFromInUse)
-                DrawFields(deckManagerEditor.deckManager.inUsePile);
+                DrawFields(_deckManagerEditor.deckManager.inUsePile);
 
             EditorGUILayout.Space();
         }
